95645abd96dc9e57db535870b3d3af8c03748583
Grace Mae <Grace Mae@DESKTOP-R8GUJV2>
2025-02-25 11:16:09 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IT15_Trojan_B.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
